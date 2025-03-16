0fd245e41899b33175e30cc43874f0bb867c46e1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2025-03-15 23:13:10 -0700

    Remove Programs class and its belonging component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,20 +6,17 @@ import Missions from "./Missions"
 import Create from "./Create";
 import "../stylesheets/app.css"
 import Astronauts from "./Astronauts";
-import Programs from "./Programs";
 
 function App() {
 
   const [ missions, setMissions ] = useState([])
   const [ astronauts, setAstronauts ] = useState([])
-  const [ programs, setPrograms ] = useState([])  
 
   return (
     <div className="App">      
       <Navbar/>
       <Routes>
         <Route path="/" element={ <Home />}/>
-        <Route path="/programs" element={ <Programs programs={programs} setPrograms={setPrograms}/>}/> 
         <Route path="/missions" element={ <Missions missions={missions} setMissions={setMissions}/>}/>        
         <Route path="/astronauts" element={ <Astronauts astronauts={astronauts} setAstronauts={setAstronauts}/>}/>
         <Route exact path="/missions/:id" element={ <Create />}/>
